7b4d92239be72ccb3c6b4f7a2a1e85cdfbef17ee
Juan Contreas <juandiegocg@ufps.edu.co>
2025-05-29 11:35:17 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
+import { clsx, type ClassValue } from "clsx"aa
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
